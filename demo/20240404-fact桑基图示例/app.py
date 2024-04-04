2a6c7fd1b9b34e90ef114ecf48304fc2e78cb481
xuzhanhong <xzh951220@163.com>
2024-04-04 18:31:45 +0800

    新增桑基图

---
+++ demo/20240404-fact桑基图示例/app.py
@@ -0,0 +1,100 @@
+import dash
+from dash import html
+import feffery_antd_charts as fact
+
+app = dash.Dash(__name__)
+
+demo_data = [
+    {
+        "source": "首次打开",
+        "target": "首页 UV",
+        "value": 160
+    },
+    {
+        "source": "结果页",
+        "target": "首页 UV",
+        "value": 40
+    },
+    {
+        "source": "验证页",
+        "target": "首页 UV",
+        "value": 10
+    },
+    {
+        "source": "我的",
+        "target": "首页 UV",
+        "value": 10
+    },
+    {
+        "source": "朋友",
+        "target": "首页 UV",
+        "value": 8
+    },
+    {
+        "source": "其他来源",
+        "target": "首页 UV",
+        "value": 27
+    },
+    {
+        "source": "首页 UV",
+        "target": "理财",
+        "value": 30
+    },
+    {
+        "source": "首页 UV",
+        "target": "扫一扫",
+        "value": 40
+    },
+    {
+        "source": "首页 UV",
+        "target": "服务",
+        "value": 35
+    },
+    {
+        "source": "首页 UV",
+        "target": "蚂蚁森林",
+        "value": 25
+    },
+    {
+        "source": "首页 UV",
+        "target": "跳失",
+        "value": 10
+    },
+    {
+        "source": "首页 UV",
+        "target": "借呗",
+        "value": 30
+    },
+    {
+        "source": "首页 UV",
+        "target": "花呗",
+        "value": 40
+    },
+    {
+        "source": "首页 UV",
+        "target": "其他流向",
+        "value": 45
+    }
+]
+
+app.layout = html.Div(
+    [
+        # 参考antd charts v1版本官网示例：https://ant-design-charts-v1.antgroup.com/zh/examples/relation-plots/sankey/#alipay
+        fact.AntdSankey(
+            data=demo_data,
+            sourceField='source',
+            targetField='target',
+            weightField='value',
+            nodeWidthRatio=0.008,
+            nodePaddingRatio=0.03,
+            nodeDraggable=True,
+            height=600
+        )
+    ],
+    style={
+        'padding': 50
+    }
+)
+
+if __name__ == '__main__':
+    app.run(debug=True)
